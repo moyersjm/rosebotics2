8348aa686fcdc6c21ee695458ddaaad43cf88990
Liam Enneking <ennekil@rose-hulman.edu>
2018-10-28 19:30:37 -0400

    robot stops when is sees designated color

--- src/Liam_Enneking.py
+++ src/Liam_Enneking.py
@@ -9,12 +9,19 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
+    stop_on_color(5)  # Red
 
+
+def stop_on_color(color, seconds=100):
+    start = time.time()
     robot = rb.Snatch3rRobot()
     robot.drive_system.start_moving()
 
     while True:
-        if robot.color_sensor.get_color() == 5:  # Red
+        if robot.color_sensor.get_color() == color:
+            robot.drive_system.stop_moving()
+            break
+        if time.time() - start > seconds:
             robot.drive_system.stop_moving()
             break
 
