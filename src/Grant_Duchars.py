8776b086f1d4914efc2dd3257f567685589609ac
Grant Duchars <grant.duchars@me.com>
2018-11-01 09:32:40 -0400

    Changed source for Grant_Duchars

--- src/Grant_Duchars.py
+++ src/Grant_Duchars.py
@@ -3,13 +3,14 @@
   Fall term, 2018-2019.
 """
 
-import rosebotics as rb
+import rosebotics_new as rb
 import time
 
 
 def main():
     """ Runs YOUR specific part of the project """
-    polygon_drive(5)
+    # polygon_drive(5)
+    run_test_arm()
 
 def polygon_drive(n):
     """Makes the robot drive in a polygon shape with n sides"""
@@ -18,6 +19,14 @@ def polygon_drive(n):
     for k in range(n):
       robot.drive_system.go_straight_inches(10)
       robot.drive_system.spin_in_place_degrees(angle)
+
+def run_test_arm():
+    robot = rb.Snatch3rRobot()
+    robot.arm.calibrate()
+    # time.sleep(1)
+    # robot.arm.raise_arm_and_close_claw()
+    # time.sleep(1)
+    # robot.arm.move_arm_to_position(300)
 
 
 main()
