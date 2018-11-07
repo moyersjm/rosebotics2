0a974f57111eb81d285dc857dc3a402b8a4b725e
moyersjm <moyersjm@rose-hulman.edu>
2018-11-07 08:14:23 -0500

    Tried to do stuff

--- src/Jonathan_Moyers.py
+++ src/Jonathan_Moyers.py
@@ -8,12 +8,37 @@ import time
 import ev3dev.ev3 as ev3
 import tkinter
 from tkinter import ttk
+import mqtt_remote_method_calls as com
+
+class MyDelegate(object):
+    def __init__(self):
+        self.robot = rb.Snatch3rRobot()
+        self.client = None
+
+    def turnandgo(self):
+        self.robot.drive_system.left_wheel.start_spinning(100)
+        self.robot.drive_system.right_wheel.start_spinning(-100)
+        while self.robot.beacon_sensor.get_heading_to_beacon() > 1:
+            time.sleep(.001)
+        self.robot.drive_system.right_wheel.start_spinning(100)
+        while self.robot.beacon_sensor.get_distance_to_beacon() > 1:
+            time.sleep(.001)
+        self.robot.drive_system.right_wheel.stop_spinning()
+        self.robot.drive_system.left_wheel.stop_spinning()
+        return
 
 
 def main():
     """ Runs YOUR specific part of the project """
     # go_along_line('left', 15)
     # camera_detect(15)
+    """my_delegate = MyDelegate()
+    mqtt_client = com.MqttClient(my_delegate)
+    my_delegate.mqtt_client = mqtt_client
+    mqtt_client.connect_to_pc()
+    while True:
+        time.sleep(0.01)"""
+    turn_to_beacon()
 
 
 def go_along_line(direction="left", seconds=30):
@@ -46,30 +71,21 @@ def camera_detect(seconds=15):
 
 
 def turn_to_beacon():
-    robot = rb.Snatch3rRobot()
     root = tkinter.Tk()
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_ev3()
 
     frame1 = ttk.Frame(root, padding=100)
     frame1.grid()
 
     button = ttk.Button(frame1, text='Infrared Beacon')
-    button['command'] = (lambda: turnandgo(robot))
+    button['command'] = (lambda: mqtt_client.send_message("turnandgo"))
     button.grid()
 
     root.mainloop()
 
 
-def turnandgo(robot):
-    robot.drive_system.left_wheel.start_spinning(100)
-    robot.drive_system.right_wheel.start_spinning(-100)
-    while robot.beacon_sensor.get_heading_to_beacon() > 1:
-        time.sleep(.001)
-    robot.drive_system.right_wheel.start_spinning(100)
-    while robot.beacon_sensor.get_distance_to_beacon() > 1:
-        time.sleep(.001)
-    robot.drive_system.right_wheel.stop_spinning()
-    robot.drive_system.left_wheel.stop_spinning()
-    return
+
 
 
 main()
--- src/m4.py
+++ src/m4.py
@@ -3,12 +3,38 @@
   Fall term, 2018-2019.
 """
 
-import rosebotics as rb
+import rosebotics_new as rb
 import time
+import mqtt_remote_method_calls as com
 
+
+class MyDelegate(object):
+    def __init__(self):
+        self.robot = rb.Snatch3rRobot()
+        self.client = None
+
+    def turnandgo(self):
+        self.robot.drive_system.left_wheel.start_spinning(100)
+        self.robot.drive_system.right_wheel.start_spinning(-100)
+        while self.robot.beacon_sensor.get_heading_to_beacon() > 1:
+            time.sleep(.001)
+        self.robot.drive_system.right_wheel.start_spinning(100)
+        while self.robot.beacon_sensor.get_distance_to_beacon() > 1:
+            time.sleep(.001)
+        self.robot.drive_system.right_wheel.stop_spinning()
+        self.robot.drive_system.left_wheel.stop_spinning()
+        return
 
 def main():
     """ Runs YOUR specific part of the project """
+    my_delegate = MyDelegate()
+    mqtt_client = com.MqttClient(my_delegate)
+    my_delegate.mqtt_client = mqtt_client
+    mqtt_client.connect_to_pc()
+    while True:
+        time.sleep(0.01)
+
+
 
 
 main()
