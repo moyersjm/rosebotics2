1e72245a91255c71cafe60240d0014c8ba8a5b0b
Grant Duchars <grant.duchars@me.com>
2018-11-07 08:55:45 -0500

    Editing capstone 2

---
+++ src/grant_duchars_runs_on_laptop.py
@@ -0,0 +1,49 @@
+"""
+Mini-application:  Buttons on a Tkinter GUI tell the robot to:
+  - Go forward at the speed given in an entry box.
+
+This module runs on your LAPTOP.
+It uses MQTT to SEND information to a program running on the ROBOT.
+
+Authors:  David Mutchler, his colleagues, and Grant Duchars.
+"""
+
+import tkinter
+from tkinter import ttk
+import mqtt_remote_method_calls as com
+
+
+def main():
+    """ Constructs and runs a GUI for this program. """
+    root = tkinter.Tk()
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_ev3()
+    setup_gui(root,mqtt_client)
+    root.mainloop()
+
+
+def setup_gui(root_window, client):
+    """ Constructs and sets up widgets on the given window. """
+    frame = ttk.Frame(root_window, padding=10)
+    frame.grid()
+
+    speed_entry_box = ttk.Entry(frame)
+    go_forward_button = ttk.Button(frame, text="Go forward")
+
+    speed_entry_box.grid()
+    go_forward_button.grid()
+
+    go_forward_button['command'] = \
+        lambda: handle_go_forward(speed_entry_box, client)
+
+
+def handle_go_forward(entry, client):
+    """
+    Tells the robot to go forward at the speed specified in the given entry box.
+    """
+    speed = entry.get()
+    client.send_message('go_forward',[speed])
+    print('Sending go_forward to the robot with a speed:',speed)
+    
+
+main()
--- src/grant_duchars_runs_on_robot.py
+++ src/grant_duchars_runs_on_robot.py
@@ -7,17 +7,8 @@ Also: responds to Beacon button-presses by beeping, speaking.
 This module runs on the ROBOT.
 It uses MQTT to RECEIVE information from a program running on the LAPTOP.
 
-Authors:  David Mutchler, his colleagues, and PUT_YOUR_NAME_HERE.
+Authors:  David Mutchler, his colleagues, and Grant Duchars.
 """
-# ------------------------------------------------------------------------------
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.  Then delete this TODO.
-# ------------------------------------------------------------------------------
-
-# ------------------------------------------------------------------------------
-# TODO: 2. With your instructor, review the "big picture" of laptop-robot
-# TODO:    communication, per the comment in mqtt_sender.py.
-# TODO:    Once you understand the "big picture", delete this TODO.
-# ------------------------------------------------------------------------------
 
 import rosebotics_new as rb
 import time
@@ -26,16 +17,10 @@ import ev3dev.ev3 as ev3
 
 
 def main():
-    # --------------------------------------------------------------------------
-    # TODO: 3. Construct a Snatch3rRobot.  Test.  When OK, delete this TODO.
-    # --------------------------------------------------------------------------
-
-    # --------------------------------------------------------------------------
-    # TODO: 4. Add code that constructs a   com.MqttClient   that will
-    # TODO:    be used to receive commands sent by the laptop.
-    # TODO:    Connect it to this robot.  Test.  When OK, delete this TODO.
-    # --------------------------------------------------------------------------
-
+    robot = rb.Snatch3rRobot()
+    
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_pc()
     # --------------------------------------------------------------------------
     # TODO: 5. Add a class for your "delegate" object that will handle messages
     # TODO:    sent from the laptop.  Construct an instance of the class and
