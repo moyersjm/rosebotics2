d546a308c4c2f59f43a66543f7a04d30485f11a3
Grant Duchars <grant.duchars@me.com>
2018-11-08 08:21:12 -0500

    Testing move to position more

--- src/Grant_Duchars.py
+++ src/Grant_Duchars.py
@@ -27,7 +27,10 @@ def run_test_arm():
     # time.sleep(1)
     # robot.arm.raise_arm_and_close_claw()
     # time.sleep(1)
-    # robot.arm.move_arm_to_position(300)
+    print('Testing Move to Position 1')
+    robot.arm.move_arm_to_position(300)
+    print('Testing Move to Position 2')
+    robot.arm.move_arm_to_position(100)
 
 
 main()
