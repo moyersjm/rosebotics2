c2db5d6e328b1a10e8ed63560e09a7af95ee0422
Grant Duchars <grant.duchars@me.com>
2018-11-08 08:45:31 -0500

    Added tests for all systems I made

--- src/Grant_Duchars.py
+++ src/Grant_Duchars.py
@@ -9,23 +9,19 @@ from ev3dev import ev3
 
 def main():
     """ Runs YOUR specific part of the project """
-    # polygon_drive(5)
-    run_test_arm()
-
-def polygon_drive(n):
-    """Makes the robot drive in a polygon shape with n sides"""
     robot = rb.Snatch3rRobot()
+    robot.drive_system.spin_in_place_degrees(360)
+    polygon_drive(robot, 5)
+    run_test_arm(robot)
+
+def polygon_drive(robot, n):
+    """Makes the robot drive in a polygon shape with n sides"""
     angle = 360 / n
     for k in range(n):
       robot.drive_system.go_straight_inches(10)
       robot.drive_system.spin_in_place_degrees(angle)
 
-# def test_buttons():
-#     robot = rb.Snatch3rRobot()
-
-def run_test_arm():
-    robot = rb.Snatch3rRobot()
-    time.sleep(1)
+def run_test_arm(robot):
     robot.arm.calibrate()
     time.sleep(1)
     robot.arm.raise_arm_and_close_claw()
