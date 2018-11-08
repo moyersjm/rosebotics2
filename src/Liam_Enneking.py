a5b405fe34242e9cbe2b7b212ce80d80ce3ffb0e
Liam Enneking <ennekil@rose-hulman.edu>
2018-11-08 08:41:46 -0500

    null

--- src/Liam_Enneking.py
+++ src/Liam_Enneking.py
@@ -31,12 +31,11 @@ def robot_beep_with_wave(seconds=15):
     robot = rb.Snatch3rRobot()
     start = time.time()
     sensor = robot.proximity_sensor
-    robot.drive_system.start_moving()
 
     while True:
         print(sensor.get_distance_to_nearest_object_in_inches())
-        if sensor.get_distance_to_nearest_object_in_inches() <= 5:
-            robot.drive_system.stop_moving()
+        if sensor.get_distance_to_nearest_object_in_inches() <= 7:
+            robot.Sound.beep
             break
         if time.time() - start > seconds:
             robot.drive_system.stop_moving()
