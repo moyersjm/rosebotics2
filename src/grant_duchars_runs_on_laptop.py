a10b5ced079009b1c5858f5f6592400920652b92
Grant Duchars <spam.th3.ham@gmail.com>
2018-11-11 17:07:44 -0500

    Testing final project

--- src/grant_duchars_runs_on_laptop.py
+++ src/grant_duchars_runs_on_laptop.py
@@ -24,17 +24,75 @@ def main():
 
 def setup_gui(root_window, client):
     """ Constructs and sets up widgets on the given window. """
-    frame = ttk.Frame(root_window, padding=10)
+    frame = ttk.Frame(root_window, padding=50)
     frame.grid()
 
+    # Speed entry for robot movement
     speed_entry_box = ttk.Entry(frame)
-    go_forward_button = ttk.Button(frame, text="Go forward")
+    speed_lable = ttk.Label(frame,text='Speed')
+    # Play button to start moving robot
+    play_button = ttk.Button(frame, text="Play Song")
+    # Stop button to stop the robot
+    stop_button = ttk.Button(frame, text='Stop')
+    # Set Notes button for send note values to robot
+    set_notes_button = ttk.Button(frame, text="Set Notes")
 
-    speed_entry_box.grid()
-    go_forward_button.grid()
+    # Entry boxes with a lable for each of the six notes
+    # (Entry boxes are filled with default values for C major)
+    note1_entry_box = ttk.Entry(frame)
+    note1_entry_box.insert(0, '261.63')
+    note1_lable = ttk.Label(frame,text='Note 1')
+    note2_entry_box = ttk.Entry(frame)
+    note2_entry_box.insert(0, '293.665')
+    note2_lable = ttk.Label(frame,text='Note 2')
+    note3_entry_box = ttk.Entry(frame)
+    note3_entry_box.insert(0, '329.628')
+    note3_lable = ttk.Label(frame,text='Note 3')
+    note4_entry_box = ttk.Entry(frame)
+    note4_entry_box.insert(0, '349.228')
+    note4_lable = ttk.Label(frame,text='Note 4')
+    note5_entry_box = ttk.Entry(frame)
+    note5_entry_box.insert(0, '391.995')
+    note5_lable = ttk.Label(frame,text='Note 5')
+    note6_entry_box = ttk.Entry(frame)
+    note6_entry_box.insert(0, '440')
+    note6_lable = ttk.Label(frame,text='Note 6')
+    
+    # Draws the elements on the screen in specific rows and columns
+    speed_entry_box.grid(row=8,column=0)
+    speed_lable.grid(row=7,column=0)
+    play_button.grid(row=8,column=1)
+    stop_button.grid(row=9,column=1)
+    set_notes_button.grid(row=6,column=1)
 
-    go_forward_button['command'] = \
+    note1_lable.grid(row=0,column=0)
+    note1_entry_box.grid(row=1,column=0)
+    note2_lable.grid(row=0,column=1)
+    note2_entry_box.grid(row=1,column=1)
+    note3_lable.grid(row=2,column=0)
+    note3_entry_box.grid(row=3,column=0)
+    note4_lable.grid(row=2,column=1)
+    note4_entry_box.grid(row=3,column=1)
+    note5_lable.grid(row=4,column=0)
+    note5_entry_box.grid(row=5,column=0)
+    note6_lable.grid(row=4,column=1)
+    note6_entry_box.grid(row=5,column=1)
+    
+    # Gives buttons fuctions the robot can perform
+    play_button['command'] = \
         lambda: handle_go_forward(speed_entry_box, client)
+    
+    stop_button['command'] = \
+        lambda: handle_stop_robot(client)
+
+    set_notes_button['command'] = \
+        lambda: handle_set_notes(note1_entry_box,
+                                  note2_entry_box,
+                                  note3_entry_box,
+                                  note4_entry_box,
+                                  note5_entry_box,
+                                  note6_entry_box,
+                                  client)
 
 
 def handle_go_forward(entry, client):
@@ -44,6 +102,25 @@ def handle_go_forward(entry, client):
     speed = entry.get()
     client.send_message('go_forward',[speed])
     print('Sending go_forward to the robot with a speed:',speed)
+
+def handle_stop_robot(client):
+    """
+    Tells the robot to stop.
+    """
+    client.send_message('stop_robot')
+    print('Sending stop_robot to the robot')
     
+def handle_set_notes(entry1,entry2,entry3,entry4,entry5,entry6, client):
+    """
+    Tells the robot to set the notes with the given values from the notes' entry boxes.
+    """
+    note1 = entry1.get()
+    note2 = entry2.get()
+    note3 = entry3.get()
+    note4 = entry4.get()
+    note5 = entry5.get()
+    note6 = entry6.get()
+    client.send_message('set_notes',[note1,note2,note3,note4,note5,note6])
+    print('Sending set_notes to the robot with the notes:',note1,note2,note3,note4,note5,note6)
 
 main()
--- src/grant_duchars_runs_on_robot.py
+++ src/grant_duchars_runs_on_robot.py
@@ -26,10 +26,25 @@ def main():
     
     while True:
         time.sleep(0.01)  # For the delegate to do its work
-        if robot.beacon_button_sensor.is_top_red_button_pressed():
-            ev3.Sound.beep().wait()
-        elif robot.beacon_button_sensor.is_top_blue_button_pressed():
-            ev3.Sound.speak('Greetings Humans').wait()
+
+        # Tests for color and plays a tone with specific frequency based on color
+        if robot.color_sensor.get_color() == 5 and rc.song_active == 1:
+            ev3.Sound.tone(rc.notes_list[0],500)
+        elif robot.color_sensor.get_color() == 3 and rc.song_active == 1:
+            ev3.Sound.tone(rc.notes_list[1],500)
+        elif robot.color_sensor.get_color() == 2 and rc.song_active == 1:
+            ev3.Sound.tone(rc.notes_list[2],500)
+        elif robot.color_sensor.get_color() == 4 and rc.song_active == 1:
+            ev3.Sound.tone(rc.notes_list[3],500)
+        elif robot.color_sensor.get_color() == 7 and rc.song_active == 1:
+            ev3.Sound.tone(rc.notes_list[4],500)
+        elif robot.color_sensor.get_color() == 1 and rc.song_active == 1:
+            ev3.Sound.tone(rc.notes_list[5],500)
+
+        # Tests for an object in front of the robot and stops if within 2 inches
+        if robot.proximity_sensor.get_distance_to_nearest_object_in_inches() <= 2:
+            robot.drive_system.stop_moving()
+            rc.song_active = 0
 
 class RemoteControlEtc(object):
 
@@ -38,11 +53,27 @@ class RemoteControlEtc(object):
         Stores a robot.
           :type robot: rb.Snatch3rRobot
         """
-        self.robot = robot
+
+    def set_notes(self, note1Str, note2Str, note3Str, note4Str, note5Str, note6Str):
+        """Sets values for six notes and stores them into a list to be called upon later"""
+        note1 = int(note1Str)
+        note2 = int(note2Str)
+        note3 = int(note3Str)
+        note4 = int(note4Str)
+        note5 = int(note5Str)
+        note6 = int(note6Str)
+        self.notes_list = [note1,note2,note3,note4,note5,note6]
 
     def go_forward(self, speed_string):
+        """Makes the robot move forward until stopped elsewhere"""
         speed = int(speed_string)
         self.robot.drive_system.start_moving(speed, speed)
+        self.song_active = 1
+    
+    def stop_robot(self):
+        """Stops the robot"""
+        self.robot.drive_system.stop_moving()
+        self.song_active = 0
 
 
 main()
